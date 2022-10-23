60a71d6551a3c59916dc25a245b39382df4879fb
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2022-10-23 00:31:20 -0500

    Fix: Run script doesn't actually start the project.

--- tools/build_and_run_editor.py
+++ tools/build_and_run_editor.py
@@ -9,7 +9,7 @@ cwdhack.cwdhack()
 def run():
     build_editor.run()
     
-    subprocess.run(["godot/bin/godot.windows.opt.tools.x86_64.mono.exe", "project"])
+    subprocess.run(["godot/bin/godot.windows.opt.tools.x86_64.mono.exe", "project/project.godot"])
 
 if __name__ == '__main__':
     run()
