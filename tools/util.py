effcbd71f7c6d521fbd737ba0eb0b82e39efd315
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2022-11-30 22:10:35 -0600

    Fix: Windows tools don't build properly.

--- tools/util.py
+++ tools/util.py
@@ -26,4 +26,4 @@ def run(command, **kwargs):
 def godot_bin():
     return platformswitch(
         linux = "bin/godot.linuxbsd.editor.x86_64.mono",
-        windows = "bin/godot.windows.editor.x86_64.mono.exe")
+        windows = "bin\\godot.windows.editor.x86_64.mono.exe")
