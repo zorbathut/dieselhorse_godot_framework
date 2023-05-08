1efd466232e921a9d836078440e0e3ecf37fbc4c
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2023-05-08 11:11:12 -0500

    Add property failure warning.

--- godot/core/object/object.cpp
+++ godot/core/object/object.cpp
@@ -301,6 +301,10 @@ void Object::set(const StringName &p_name, const Variant &p_value, bool *r_valid
 	if (r_valid) {
 		*r_valid = false;
 	}
+	else
+	{
+		WARN_PRINT("Failed to set property " + get_class_name() + "::" + p_name);
+	}
 }
 
 Variant Object::get(const StringName &p_name, bool *r_valid) const {
@@ -384,6 +388,11 @@ Variant Object::get(const StringName &p_name, bool *r_valid) const {
 		if (r_valid) {
 			*r_valid = false;
 		}
+		else
+		{
+			WARN_PRINT("Failed to get property " + get_class_name() + "::" + p_name);
+		}
+		
 		return Variant();
 	}
 }
