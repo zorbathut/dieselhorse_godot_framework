295fc47e9e37639bd6890b64083ed8f407673dbe
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-05-13 20:58:22 -0500

    Build images properly?

---
+++ build/godot-build-containers/msb.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+basedir=$(cd $(dirname "$0"); pwd)
+
+source $basedir/setup.sh
+
+img_version=$GBCMSB_ID
+files_root="$basedir/files"
+
+mkdir -p logs
+
+"$podman" build -t godot-fedora:${img_version} -f Dockerfile.base . 2>&1 | tee logs/base.log
+
+podman_build() {
+  # You can add --no-cache as an option to podman_build below to rebuild all containers from scratch.
+  "$podman" build \
+    --build-arg img_version=${img_version} \
+    -v "${files_root}":/root/files:z \
+    -t godot-"$1:${img_version}" \
+    -f Dockerfile."$1" . \
+    2>&1 | tee logs/"$1".log
+}
+
+podman_build linux
+podman_build windows
