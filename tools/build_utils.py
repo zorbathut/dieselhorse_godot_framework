eae4e66a1f586f2c98fc2639121541f903446201
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-06-18 11:45:47 -0500

    Hopefully get builds working on Linux.

--- tools/build_utils.py
+++ tools/build_utils.py
@@ -5,6 +5,11 @@ def get_env():
     """Get the environment variables for the current platform."""
     env = os.environ.copy()
     env['GODOT_VERSION_STATUS'] = 'moonskrive'
+
+    # use the build-environment path for Linux builds
+    if "GODOT_SDK_LINUX_X86_64" in env:
+        env["PATH"] = env["GODOT_SDK_LINUX_X86_64"] + "/bin" + ':' + env["PATH"]
+
     return env
 
 def get_float_precision():
