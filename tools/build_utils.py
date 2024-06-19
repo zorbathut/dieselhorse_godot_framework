4e5e748d3098d95f56da01dbfe2303df99f1d7c9
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-06-19 00:35:31 -0500

    More diagnostics?

--- tools/build_utils.py
+++ tools/build_utils.py
@@ -1,5 +1,6 @@
 
 import os
+import pprint
 
 def get_env():
     """Get the environment variables for the current platform."""
@@ -7,6 +8,8 @@ def get_env():
     env['GODOT_VERSION_STATUS'] = 'moonskrive'
 
     # use the build-environment path for Linux builds
+    print("GETENV RESULTS")
+    pprint.pprint(env)
     if "GODOT_SDK_LINUX_X86_64" in env:
         print(f"Using Linux SDK path: {env["GODOT_SDK_LINUX_X86_64"]}")
         env["PATH"] = env["GODOT_SDK_LINUX_X86_64"] + "/bin" + ':' + env["PATH"]
