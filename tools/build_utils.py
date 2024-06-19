a0641b7501343ddb03f8d011bf9dcc19d0774fdd
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-06-19 02:22:32 -0500

    Remove debug stuff.

--- tools/build_utils.py
+++ tools/build_utils.py
@@ -8,18 +8,10 @@ def get_env():
     env['GODOT_VERSION_STATUS'] = 'moonskrive'
 
     # use the build-environment path for Linux builds
-    print("GETENV RESULTS")
     pprint.pprint(env)
     if "GODOT_SDK_LINUX_X86_64" in env:
         print(f"Using Linux SDK path: {env["GODOT_SDK_LINUX_X86_64"]}")
-        # print all files in the SDK directory
-        print("Files in SDK directory:")
-        for root, dirs, files in os.walk(env["GODOT_SDK_LINUX_X86_64"]):
-            for file in files:
-                print(os.path.join(root, file))
-
         env["PATH"] = env["GODOT_SDK_LINUX_X86_64"] + "/bin" + ':' + env["PATH"]
-        print(f"New PATH: {env["PATH"]}")
 
     return env
 
