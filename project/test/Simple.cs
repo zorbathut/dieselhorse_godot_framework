53874bbe0daef8053bcc04991d01c1db27f293e4
Ben Rog-Wilhelm <zorba-git-sharon-vfio@pavlovian.net>
2024-07-22 09:57:45 -0500

    Implement first actual game event unit tests.

--- project/test/Simple.cs
+++ project/test/Simple.cs
@@ -1,4 +1,5 @@
 
+using System.Linq;
 using GdUnit4;
 
 namespace Test;
@@ -25,6 +26,44 @@ public class Simple : Base
     {
         Init();
 
-        var context = new Foundation.Coordinator(Foundation.ContextUtil.CreateMap());
+        var playerInputShunt = new ReadWriteLockedResource<Foundation.SharedPlayerInput>(new Foundation.SharedPlayerInput());
+        var context = new Foundation.Executor(playerInputShunt) { context = Foundation.ContextUtil.CreateMap() };
+    }
+
+    [TestCase]
+    public void NullFrame()
+    {
+        Init();
+
+        var playerInputShunt = new ReadWriteLockedResource<Foundation.SharedPlayerInput>(new Foundation.SharedPlayerInput());
+        var context = new Foundation.Executor(playerInputShunt) { context = Foundation.ContextUtil.CreateMap() };
+
+        context.Process();
+    }
+
+    [TestCase]
+    public void SpawnAndRun()
+    {
+        Init();
+
+        var playerInputShunt = new ReadWriteLockedResource<Foundation.SharedPlayerInput>(new Foundation.SharedPlayerInput());
+        var context = new Foundation.Executor(playerInputShunt) { context = Foundation.ContextUtil.CreateMap() };
+
+        using var envscope = new Ghi.Environment.Scope(context.context.env);
+
+        {
+            using var writeable = playerInputShunt.Write();
+            var frame = new Foundation.InputAccumulator.Frame();
+            frame.events = new System.Collections.Generic.List<Foundation.GlobalEvent>();
+            frame.events.Add(new Foundation.GlobalEventSpawn() { playerId = 0 });
+
+            writeable.Resource.data.Add((context.context.env.SingletonRO<Comp.Global>().timeFrame + 1, frame));
+        }
+
+        Assert.AreEqual(0, context.context.env.List.Count(e => e.HasComponent<Comp.Player>()));
+
+        context.Process();
+
+        Assert.AreEqual(1, context.context.env.List.Count(e => e.HasComponent<Comp.Player>()));
     }
 }
