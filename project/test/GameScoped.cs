4370d5351e3f0508e871efea69c877a09e9a83f9
Ben Rog-Wilhelm <zorba-git-sharon-vfio@pavlovian.net>
2024-07-22 10:36:03 -0500

    Package up some testing functionality into a helper class.

---
+++ project/test/GameScoped.cs
@@ -0,0 +1,50 @@
+
+using System;
+
+namespace Test;
+
+public class GameScoped : System.IDisposable
+{
+    public Foundation.Executor executor;
+
+    public Ghi.Environment Env => executor.context.env;
+
+    private ReadWriteLockedResource<Foundation.SharedPlayerInput> playerInputShunt = new ReadWriteLockedResource<Foundation.SharedPlayerInput>(new Foundation.SharedPlayerInput());
+
+    private Ghi.Environment.Scope scope;
+
+    public GameScoped()
+    {
+        executor = new Foundation.Executor(playerInputShunt) { context = Foundation.ContextUtil.CreateMap() };
+
+        scope = new Ghi.Environment.Scope(Env);
+    }
+
+    public void Process(Comp.PlayerInput input, Foundation.GlobalEvent globalEvent = null)
+    {
+        // just in case we're not scoped
+        using var envscope = new Ghi.Environment.Scope(Env);
+
+        {
+            using var writeable = playerInputShunt.Write();
+
+            var frame = new Foundation.InputAccumulator.Frame();
+            frame.inputs[0] = input;
+
+            if (globalEvent != null)
+            {
+                frame.events = new();
+                frame.events.Add(globalEvent);
+            }
+
+            writeable.Resource.data.Add((Env.SingletonRO<Comp.Global>().timeFrame + 1, frame));
+        }
+
+        executor.Process();
+    }
+
+    public void Dispose()
+    {
+        scope.Dispose();
+    }
+}
--- project/test/Simple.cs
+++ project/test/Simple.cs
@@ -46,24 +46,12 @@ public class Simple : Base
     {
         Init();
 
-        var playerInputShunt = new ReadWriteLockedResource<Foundation.SharedPlayerInput>(new Foundation.SharedPlayerInput());
-        var context = new Foundation.Executor(playerInputShunt) { context = Foundation.ContextUtil.CreateMap() };
+        using var gameScoped = new GameScoped();
 
-        using var envscope = new Ghi.Environment.Scope(context.context.env);
+        Assert.AreEqual(0, gameScoped.Env.List.Count(e => e.HasComponent<Comp.Player>()));
 
-        {
-            using var writeable = playerInputShunt.Write();
-            var frame = new Foundation.InputAccumulator.Frame();
-            frame.events = new System.Collections.Generic.List<Foundation.GlobalEvent>();
-            frame.events.Add(new Foundation.GlobalEventSpawn() { playerId = 0 });
+        gameScoped.Process(new Comp.PlayerInput(), new Foundation.GlobalEventSpawn() { playerId = 0 });
 
-            writeable.Resource.data.Add((context.context.env.SingletonRO<Comp.Global>().timeFrame + 1, frame));
-        }
-
-        Assert.AreEqual(0, context.context.env.List.Count(e => e.HasComponent<Comp.Player>()));
-
-        context.Process();
-
-        Assert.AreEqual(1, context.context.env.List.Count(e => e.HasComponent<Comp.Player>()));
+        Assert.AreEqual(1, gameScoped.Env.List.Count(e => e.HasComponent<Comp.Player>()));
     }
 }
