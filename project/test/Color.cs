c39e7f063b0e21f191eb15263833678c1e1ec92a
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-07-29 01:25:59 -0500

    Add color tests.

---
+++ project/test/Color.cs
@@ -0,0 +1,31 @@
+
+using System.Linq;
+using GdUnit4;
+
+namespace Test;
+
+[TestSuite]
+public class Color : Base
+{
+    [TestCase]
+    public void OKLCH()
+    {
+        base.Init();
+
+        var cc = new Converter.ColorConverter();
+
+        {
+            var c = cc.Read("oklch(70% 0.1 188)", new Dec.InputContext("test"));
+            Assert.AreEqual(0.05818826f, c.R);
+            Assert.AreEqual(0.44735658f, c.G);
+            Assert.AreEqual(0.40130332f, c.B);
+        }
+
+        {
+            var c = cc.Read("oklch(64.39% 0.0781 79)", new Dec.InputContext("test"));
+            Assert.AreEqual(0.38660142f, c.R);
+            Assert.AreEqual(0.24581487f, c.G);
+            Assert.AreEqual(0.09080556f, c.B);
+        }
+    }
+}
