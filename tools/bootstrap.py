cf88f7c19b46aa9d6efa16d41aa1b5b6f115b9ec
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-09-20 13:31:10 -0500

    Unbuffer the bootstrap better.

--- tools/bootstrap.py
+++ tools/bootstrap.py
@@ -6,6 +6,23 @@ import sys
 import tools.util
 
 def execute(token, args = [], shell = False):
+    # Explicitly unbuffer the output
+    # this is really hacky and comes from https://stackoverflow.com/questions/107705/disable-output-buffering
+    # I am not totally sold on it
+    # wish you could just tell python "hey stop buffering kthx"
+    class Unbuffered(object):
+        def __init__(self, stream):
+            self.stream = stream
+        def write(self, data):
+            self.stream.write(data)
+            self.stream.flush()
+        def writelines(self, datas):
+            self.stream.writelines(datas)
+            self.stream.flush()
+        def __getattr__(self, attr):
+            return getattr(self.stream, attr)
+    sys.stdout = Unbuffered(sys.stdout)
+    
     poetry_options = [
         shutil.which("poetry"),
         os.path.expandvars("%APPDATA%/pypoetry/venv/Scripts/poetry.exe"),   # windows default install location
