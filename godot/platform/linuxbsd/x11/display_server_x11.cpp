2cf19a669beafe055888a4890a3a7f0108602c29
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-09-25 00:29:59 -0500

    Release DisplayServer's rendering resources before destroying the associated RenderingServer
    Currently this seems to only affect textures related to custom cursors.
    
    X11 and macOS implementations were not tested.
    
    fixes #68472

--- godot/platform/linuxbsd/x11/display_server_x11.cpp
+++ godot/platform/linuxbsd/x11/display_server_x11.cpp
@@ -5199,6 +5199,10 @@ void DisplayServerX11::process_events() {
 	Input::get_singleton()->flush_buffered_events();
 }
 
+void DisplayServerX11::release_rendering_resources() {
+	cursors_cache.clear();
+}
+
 void DisplayServerX11::release_rendering_thread() {
 #if defined(GLES3_ENABLED)
 	if (gl_manager) {
@@ -6469,6 +6473,8 @@ DisplayServerX11::~DisplayServerX11() {
 		memdelete(native_menu);
 		native_menu = nullptr;
 	}
+
+	ERR_FAIL_COND_MSG(!cursors_cache.is_empty(), "Cursor cache not cleared, call release_rendering_resources before destroying the related RenderingServer.");
 
 	//destroy all windows
 	for (KeyValue<WindowID, WindowData> &E : windows) {
