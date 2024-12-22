e25e65c939edf8d512dd9d3d6216b00975c4475e
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2024-12-22 02:22:39 -0600

    Update a comment.

--- godot/scene/scene_string_names.cpp
+++ godot/scene/scene_string_names.cpp
@@ -89,7 +89,7 @@ SceneStringNames::SceneStringNames() {
 	gui_input = StaticCString::create("gui_input");
 	// DH BEGIN - gui_input_accepted_in_child
 	gui_input_accepted_in_child = StaticCString::create("gui_input_accepted_in_child");
-	// DH END
+	// DH END - gui_input_accepted_in_child
 
 	_spatial_editor_group = StaticCString::create("_spatial_editor_group");
 	_request_gizmo = StaticCString::create("_request_gizmo");
