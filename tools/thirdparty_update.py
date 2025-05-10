1e4e649e7f3ec6618606dc92cdb09190b00e12c1
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-05-10 05:41:49 -0500

    Rename some badly-named tool scripts.

--- tools/thirdparty_update.py
+++ tools/thirdparty_update.py
@@ -1,33 +1,110 @@
 
-import glob
-import os
+import argparse
 import shutil
+import sys
 import util
 
 util.cwdhack()
 
-def copyglob(sources, destination):
-    shutil.rmtree(destination, ignore_errors=True)
-    print(destination)
-    os.makedirs(destination, exist_ok=True)
+if not sys.platform.startswith("linux"):
+    print("Currently Linux-only (though may not be hard to port)")
+    raise
 
-    for source in sources:
-        print("", source)
-        source_files = glob.glob(source)
-        for file in source_files:
-            print("", "", file)
-            shutil.copy(file, destination)
+parser = argparse.ArgumentParser()
+parser.add_argument("--commit", required = True)
+args = parser.parse_args()
 
-def run():
-    copyglob(['../dec/src/*.cs'], 'project/thirdparty/dec')
-    copyglob(['../dec/extra/recorder_enumerator/src/*.cs'], 'project/thirdparty/dec/recorder_enumerator')
+if util.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output = True).stdout.decode().strip() != "dev":
+    print("Error: Not on dev branch (this is probably fixable but it'll take some work)")
+    sys.exit(1)
 
-    copyglob(['../ghi/src/*.cs'], 'project/thirdparty/ghi')
+util.run([
+        "git",
+        "clone",
+        ".",
+        "update_godot",
+    ], check=True)
 
-    copyglob(['../arbor/src/*.cs'], 'project/thirdparty/arbor')
-    copyglob(['../arbor/arbor-generator/*.cs', '../arbor/arbor-generator/*.csproj'], 'project/thirdparty/arbor/arbor-generator')
+util.run([
+        "git",
+        "checkout",
+        "thirdparty_godot",
+    ], cwd = "update_godot", check=True)
 
-    util.run(["dotnet", "restore"], check=True)
+util.run([
+        "git",
+        "clone",
+        "--depth", "1",
+        "--branch", args.commit,
+        "https://github.com/godotengine/godot.git",
+        "update_godot_engine",
+    ], check=True)
 
-if __name__ == '__main__':
-    run()
+util.run([
+        "git",
+        "checkout",
+        args.commit,
+    ], cwd = "update_godot_engine", check=True)
+
+shutil.rmtree("update_godot/godot")
+shutil.copytree("update_godot_engine", "update_godot/godot")
+
+util.run([
+        "git",
+        "add",
+        "-f",
+        ".",
+    ], cwd = "update_godot", check=True)
+
+util.run([
+        "git",
+        "commit",
+        "-m",
+        f"Godot {args.commit}"
+    ], cwd = "update_godot", check=True)
+
+util.run([
+        "git",
+        "checkout",
+        "dev",
+    ], cwd = "update_godot", check=True)
+
+if util.run([
+        "git",
+        "merge",
+        "thirdparty_godot",
+    ], cwd = "update_godot").returncode != 0:
+
+    input("Merge conflicts; fix, commit, then hit enter")
+
+util.run([
+        "git",
+        "fetch",
+        "update_godot",
+        "thirdparty_godot",
+    ], check=True)
+
+util.run([
+        "git",
+        "branch",
+        "-f",
+        "thirdparty_godot",
+        "FETCH_HEAD",
+    ], check=True)
+
+util.run([
+        "git",
+        "fetch",
+        "update_godot",
+        "dev",
+    ], check=True)
+
+util.run([
+        "git",
+        "merge",
+        "FETCH_HEAD",
+        "--no-commit",
+    ], check=True)
+
+shutil.rmtree("update_godot")
+shutil.rmtree("update_godot_engine")
