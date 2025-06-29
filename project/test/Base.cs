1ac8398aaee385b0b27147de2153c624081063fd
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-06-29 17:34:28 -0500

    Fix: Tests don't work after scanning Dec parameter types.

--- project/test/Base.cs
+++ project/test/Base.cs
@@ -10,6 +10,9 @@ public class Base
     {
         LibGodot.StartIfNecessary();
 
+        // This is safe because this is all intrinsically single-threaded; we currently very much do not support multithreaded testing (maybe someday?)
+        Godot.GodotThread.SetThreadSafetyChecksEnabled(false);
+
         // find the only Bootstrap node and finish our init
         var sceneTree = Godot.Engine.GetMainLoop() as SceneTree;
         var bootstrap = sceneTree.Root.GetNode<Foundation.Bootstrap>("Bootstrap");
