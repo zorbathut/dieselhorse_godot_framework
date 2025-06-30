4404b65bb4cf7b77eb82e2c3f70b36ade30cd71f
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-06-30 10:47:06 -0500

    Fix up csproj settings.

--- project/test/lib/LibGodot.cs
+++ project/test/lib/LibGodot.cs
@@ -1,3 +1,6 @@
+
+using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 public enum GDExtensionInitializationLevel
--- project/test/lib/Util.cs
+++ project/test/lib/Util.cs
@@ -1,5 +1,7 @@
 
+using System;
 using System.Collections;
+using System.Linq;
 
 namespace Test;
 
