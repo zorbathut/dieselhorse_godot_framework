4d7d44bff8b558f2d0dd3f2a70118312a9f932e0
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-06-30 06:47:03 -0500

    Add proper Wrn/Err detection to tests.

--- project/test/Base.cs
+++ project/test/Base.cs
@@ -5,9 +5,26 @@ namespace Test;
 
 public class Base
 {
+    private static bool handlingError = false;
+
     [SetUp]
     public void SetUp()
     {
+        // Reset flags
+        handlingError = false;
+
+        // Set up our hooks right now, just in case we get errors in LibGodot
+        Foundation.Bootstrap.LogWarningSecondaryHook = msg =>
+        {
+            // if we just fail, we end up in infinite recursion
+            if (!handlingError)
+            {
+                handlingError = true;
+                NUnit.Framework.Assert.Fail(msg);
+                handlingError = false;
+            }
+        };
+
         LibGodot.StartIfNecessary();
 
         // This is safe because this is all intrinsically single-threaded; we currently very much do not support multithreaded testing (maybe someday?)
