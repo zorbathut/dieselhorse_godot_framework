f03469788820792762015dbc469ed2c96ef115f9
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-07-02 08:17:29 -0500

    Disable node reuse because it might be causing problems on Jenkins.

--- godot/modules/mono/build_scripts/build_assemblies.py
+++ godot/modules/mono/build_scripts/build_assemblies.py
@@ -354,10 +354,12 @@ def build_all(
         return exit_code
 
     # GodotTools
+    # DH BEGIN - disable node reuse because it might be causing problems on Jenkins
     sln = os.path.join(module_dir, "editor/GodotTools/GodotTools.sln")
     args = ["/restore", "/t:Build", "/p:Configuration=" + ("Debug" if dev_debug else "Release")] + (
         ["/p:GodotPlatform=" + godot_platform] if godot_platform else []
-    )
+    ) +  ["/nodeReuse:false"]
+    # DH END - disable node reuse because it might be causing problems on Jenkins
     if push_nupkgs_local:
         args += ["/p:ClearNuGetLocalCache=true", "/p:PushNuGetToLocalSource=" + push_nupkgs_local]
     if precision == "double":
